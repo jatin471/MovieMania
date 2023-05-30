29bb6095f294dfe322a6f400ff8965b80885f773
Jatin Yadav <jatinyadav471@gmail.com>
2023-05-30 09:21:57 +0530

    Initial Commit

---
+++ MovieMania/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMania
+//
+//  Created by JATIN YADAV on 30/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
